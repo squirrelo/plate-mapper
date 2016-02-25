aefbc50c7ee566dd722d3ff7c65ddc3cf0ae1eb1
Joshua Shorenstein <squirrelo@gmail.com>
2016-02-25 11:52:09 -0500

    add rest of functionality

--- platemap/lib/plate.py
+++ platemap/lib/plate.py
@@ -130,6 +130,13 @@ class Plate(PMObject):
             row, col = pos[0], pos[1]
             TRN.add(sql, [value.id, self.id, row, col])
 
+    def _get_property(self, column):
+        sql = "Select {} from barcodes.plate WHERE plate_id = %s".format(
+            column)
+        with TRN:
+            TRN.add(sql, [self.id])
+            return TRN.execute_fetchlast()
+
     @property
     def name(self):
         """Name of the plate
@@ -139,7 +146,7 @@ class Plate(PMObject):
         str
             Name of the plate
         """
-        raise NotImplementedError()
+        return self._get_property('name')
 
     @property
     def finalized(self):
@@ -150,7 +157,21 @@ class Plate(PMObject):
         bool
             If the plate is finalized (True) or not (False)
         """
-        raise NotImplementedError()
+        return self._get_property('finalized')
+
+    @property
+    def shape(self):
+        """Shaple of the plate
+
+        Returns
+        -------
+        tuple of int
+            Plate dimensions in the form (rows, cols)
+        """
+        sql = "SELECT rows, cols FROM barcodes.plate WHERE plate_id = %s"
+        with TRN:
+            TRN.add(sql, [self.id])
+            return tuple(TRN.execute_fetchindex()[0])
 
     @property
     def samples(self):
@@ -158,13 +179,71 @@ class Plate(PMObject):
 
         Returns
         -------
-        list of Sample objects or None
+        list of Sample objects
             Samples on the plate, ordered by row.
             Sample at [0, 0], followed by [0, 1], [0, 2], etc.
-            If no sample exists for the well, None is added to the list
         """
-        raise NotImplementedError()
+        sql = """SELECT sample_id
+                 FROM barcodes.plates_samples
+                 WHERE plate_id = %s
+                 ORDER BY row, col
+              """
+        with TRN:
+            TRN.add(sql, [self.id])
+            return [Sample(s) for s in TRN.execute_fetchflatten()]
+
+    @property
+    def platemap(self):
+        """Samples on the plate, mapped as list of lists
+
+        Returns
+        -------
+        list of list of Sample objects or None
+            Samples on the plate, with None if no sample at the position
+        """
+        sql = """SELECT row || col, sample_id
+                 FROM barcodes.plates_samples
+                 WHERE plate_id = %s
+                 ORDER BY row, col
+              """
+        with TRN:
+            rows, cols = self.shape
+            TRN.add(sql, [self.id])
+            # Turn the returned rows into a dict keyed to the combined
+            # rowcol created by the sql query
+            samples = dict(TRN.execute_fetchindex())
+            ret = []
+            # Loop over each sample and add None of no sample in position
+            for r in range(rows):
+                ret.append([])
+                for c in range(cols):
+                    samp = samples.get('%d%d' % (r + 1, c + 1), None)
+                    ret[r].append(Sample(samp) if samp is not None else None)
+            return ret
 
     # -------- functions ----------------
     def to_html(self):
-        raise NotImplementedError()
+        """Builds an HTML table representation of the plate
+
+        Returns
+        -------
+        str
+            HTML representation of the plate
+
+        Notes
+        -----
+        The class `plate` is added to the table for css styling.
+        """
+
+        samples = self.platemap
+        rows, cols = self.shape
+        table = ['<table class="plate">']
+        for row in range(rows):
+            table.append('<tr>')
+            for col in range(cols):
+                samp = samples[row][col]
+                table.append('<td>%s</td>' %
+                             samp.name if samp is not None else '')
+            table.append('</tr>')
+        table.append('</table>')
+        return ''.join(table)
