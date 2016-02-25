931e64b7b59b99e61e9f58de908280c22fb4b2b5
Joshua Shorenstein <squirrelo@gmail.com>
2016-02-24 23:24:49 -0500

    update test sql

--- platemap/db/populate_test.sql
+++ platemap/db/populate_test.sql
@@ -24,8 +24,8 @@ INSERT INTO barcodes.sample (sample, barcode, sample_type, sample_location, crea
 ('Sample 3', NULL, 'skin', 'the other freezer', '2016-02-22 8:54:00', 1, '2016-02-22 8:54:00', 1, 1, 'F'),
 ('Sample 4', NULL, 'oral', 'the freezer', '2016-02-22 8:55:00', 1, '2016-02-22 8:55:00', 1, 2, 'T');
 
-INSERT INTO barcodes.plate (plate_id, plate, created_on, finalized, person_id) VALUES
-('000000003', 'Test plate 1', '2016-02-22 8:55:00', 'F', 1);
+INSERT INTO barcodes.plate (plate_id, plate, created_on, finalized, person_id, rows, cols) VALUES
+('000000003', 'Test plate 1', '2016-02-22 8:55:00', 'F', 1, 8, 12);
 
 INSERT INTO barcodes.plates_samples (plate_id, sample_id, plate_row, plate_col) VALUES
 ('000000003', 1, 1, 1), ('000000003', 2, 1, 2), ('000000003', 3, 1, 3);
