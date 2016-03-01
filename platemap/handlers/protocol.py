9a4ff8bc363e5b88576ebbceb8dcfc5ccd735708
Joshua Shorenstein <squirrelo@gmail.com>
2016-02-29 16:16:05 -0800

    add logging PCR

--- platemap/handlers/protocol.py
+++ platemap/handlers/protocol.py
@@ -15,4 +15,23 @@ class LogExtractionHandler(BaseHandler):
     @authenticated
     def get(self):
         plates = pm.plate.Plate.plates(finalized=True)
-        self.render('log_extraction.html', plates=plates)
+        self.render('log_extraction.html', plates=plates, msg='')
+
+    @authenticated
+    def post(self):
+        plate_id = self.get_argument('plate-id')
+        extract_lot = self.get_argument('extractionkit_lot')
+        extract_robot = self.get_argument('extraction_robot')
+        tm_tool = self.get_argument('tm1000_8_tool')
+
+        try:
+            pm.protocol.ExtractionProtocol.create(
+                self.current_user.person, extract_lot, extract_robot,
+                tm_tool, plate=pm.plate.Plate(plate_id))
+        except Exception as e:
+            # Render any error to the user interface
+            msg = self.write(str(e))
+            plates = pm.plate.Plate.plates(finalized=True)
+            self.render('log_extraction.html', plates=plates, msg=msg)
+        else:
+            self.redirect('/?msg=Successfully+logged+PCR+run')
